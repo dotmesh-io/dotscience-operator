ca67a6982fc5e001aaa2ecb0d891dc77b5fe4788
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-12-13 16:37:23 +0000

    expanding types

--- pkg/apis/deployer/v1/deployerservice_types.go
+++ pkg/apis/deployer/v1/deployerservice_types.go
@@ -8,11 +8,39 @@ import (
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // DeployerServiceSpec defines the desired state of DeployerService
+// +k8s:openapi-gen=true
 type DeployerServiceSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// Name is deployer name
+	Name string `json:"name"`
+
+	// Image is the container image to run as the job.
+	Image string `json:"image"`
+
+	// Gateway address (dotscience.com)
+	GatewayAddress string `json:"gateway"`
+
+	// token key (generated when creating a new deployer via https://cloud.dotscience.com/deployers)
+	Token string `json:"token"`
+
+	// ServiceAccountName for the deployer to use. Must have permissions to create resources
+	ServiceAccountName string `json:"serviceAccountName"`
 }
+
+// DeployerPhase is the phase of the deployer at a given point in time.
+type DeployerPhase string
+
+// Constants for operator defaults values and different phases.
+const (
+	DeployerPhaseInitial     DeployerPhase = ""
+	DeployerPhaseRunning     DeployerPhase = "Running"
+	DeployerPhaseCreating    DeployerPhase = "Creating"
+	DeployerPhasePending     DeployerPhase = "Pending"
+	DeployerPhaseTerminating DeployerPhase = "Terminating"
+)
 
 // DeployerServiceStatus defines the observed state of DeployerService
 type DeployerServiceStatus struct {
